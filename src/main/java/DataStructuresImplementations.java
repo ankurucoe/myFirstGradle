2d6839300653cd3e3fbca354f236875c86384c4c
Ankur <ankurucoe@gmail.com>
2020-02-26 00:11:05 +0530

    Sample Code Commit

--- src/main/java/DataStructuresImplementations.java
+++ src/main/java/DataStructuresImplementations.java
@@ -1,3 +1,26 @@
-public class DataStructuresImplementations {
+import java.util.ArrayList;
 
+public class DataStructuresImplementations extends DataStructurePractice{
+
+    public int findKthSmallest(int [] arr, int n){
+        return n;
+    }
+
+    public int chocolateDistribution(){
+        ArrayList<Integer> A = new ArrayList<>();
+        A.size();
+        return 1;
+    }
+
+    public int sortingArrayDiffValues(int [] arr){
+        return 10;
+    }
+
+    public void mergeIntervals(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
