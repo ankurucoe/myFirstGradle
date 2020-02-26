e01ee72074e0e8ff9093f7fcd769549efb6d56f3
Ankur <ankurucoe@gmail.com>
2020-02-27 00:54:07 +0530

    adding Interval Overlap Function

--- src/main/java/DataStructuresImplementations.java
+++ src/main/java/DataStructuresImplementations.java
@@ -1,5 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
+class Interval
+{
+    int start,end;
+
+    Interval(int start, int end)
+    {
+        this.start=start;
+        this.end=end;
+    }
+}
 public class DataStructuresImplementations extends DataStructurePractice{
 
     public int findKthSmallest(int [] arr, int n){
@@ -16,11 +28,20 @@ public class DataStructuresImplementations extends DataStructurePractice{
         return 10;
     }
 
-    public void mergeIntervals(){
+    public void mergeIntervals(Interval [] arr){
+        Arrays.sort(arr, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o2.start-o1.start;
+            }
+        });
+        int index = 0;
+        for(int i=1;i<arr.length;i++){
 
+        }
     }
 
     public static void main(String[] args) {
-
+            Interval [] arr = new Interval[4];
     }
 }
