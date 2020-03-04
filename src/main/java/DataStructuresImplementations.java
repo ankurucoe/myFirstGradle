d1ac3fb9b63dfa4157b692c3dfb2a940ba329d0f
Ankur <ankurucoe@gmail.com>
2020-03-04 07:33:05 +0530

    adding Merger Interval decreasing Order

--- src/main/java/DataStructuresImplementations.java
+++ src/main/java/DataStructuresImplementations.java
@@ -37,11 +37,27 @@ public class DataStructuresImplementations extends DataStructurePractice{
         });
         int index = 0;
         for(int i=1;i<arr.length;i++){
-
+            if(arr[index].end >= arr[i].start){
+                arr[index].end = Math.max(arr[index].end, arr[i].end);
+                arr[index].start = Math.min(arr[index].start, arr[i].start);
+            }
+            else{
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        System.out.println("The Merger Intervals");
+        for(int j=0;j<=index;j++){
+            System.out.print(arr[j].start+" "+arr[j].end);
         }
     }
 
     public static void main(String[] args) {
             Interval [] arr = new Interval[4];
+            arr[0] = new Interval(6,8);
+            arr[1] = new Interval(1,9);
+            arr[2] = new Interval(2,4);
+            arr[3] = new Interval(4,7);
+            new DataStructuresImplementations().mergeIntervals(arr);
     }
 }
