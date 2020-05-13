354e0011123414281d6bbe525c65c3641da7a4ae
Ankur <ankurucoe@gmail.com>
2020-05-14 01:00:57 +0530

    sample

--- src/main/java/DataStructuresImplementations.java
+++ src/main/java/DataStructuresImplementations.java
@@ -672,10 +672,22 @@ public class DataStructuresImplementations extends DataStructurePractice{
         }
         return result;
     }
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        System.out.println(map.keySet()+" "+map.values());
+        return maxlen;
+    }
     public static void main(String[] args) {
-        int[] arr = {10, 5, 2, 7, 1, 9};
-        int k = 15;
-        int n = arr.length;
-        System.out.println(maxLen(arr,n,k));
+
     }
 }
