95bf4ea58c6261e46dc4e64aa4916609c6f1d119
Ankur <ankurucoe@gmail.com>
2020-06-23 00:29:24 +0530

    left over

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,7 +27,48 @@ public class App {
         }
         return s.isEmpty() ? "YES":"NO";
     }
+
+    public static void stockSpan(int [] price, int [] S, int n){
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        S[0] = 1;
+        for(int i=1;i<n;i++){
+            while(!st.empty() && price[st.peek()] <= price[i])
+                st.pop();
+
+            S[i] = (st.empty()) ? (i+1) : (i-st.peek());
+
+            st.push(i);
+
+        }
+    }
+    public void previousGreaterElement(int [] arr, int n){
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[0]);
+        System.out.print(-1+" ,");
+        for(int i=1;i<n;i++){
+            while(!st.empty() && st.peek()<arr[i]){
+                st.pop();
+            }
+            if(st.empty()==true){
+                System.out.print(-1+" ,");
+            }
+            else{
+                System.out.print(st.peek()+" ,");
+            }
+            st.push(arr[i]);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(new App().balancedParanth("{{)[](}}"));
+        //System.out.println(new App().balancedParanth("{{)[](}}"));
+        int price[] = { 10, 4, 5, 90, 120, 80 };
+        int n = price.length;
+        int [] S = new int[n];
+        stockSpan(price,S,n);
+        //calculateSpan(price, n, S);
+//        for(int i:S){
+//            System.out.println(i);
+//        }
+        new App().previousGreaterElement(price, price.length);
     }
 }
