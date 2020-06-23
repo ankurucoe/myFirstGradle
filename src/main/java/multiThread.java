fc3ee2dd36259a4e3aae4190488f964ddc0c6894
Ankur <ankurucoe@gmail.com>
2020-06-24 01:19:50 +0530

    more code

--- src/main/java/multiThread.java
+++ src/main/java/multiThread.java
@@ -1,19 +1,56 @@
 public class multiThread implements Runnable{
     public void run(){
         try{
-            System.out.println(Thread.currentThread().getId()+" id Running");
+            Thread.sleep(1500);
+            System.out.println(Thread.currentThread().getId()+" Thread Sleeping");
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
-    }
+        System.out.println(Test.thread1.getState()+"  State after Sleep function");
 
-    public static void main(String[] args) {
-        int n = 8;
-        for(int i=0;i<n;i++){
-            Thread m = new Thread(new multiThread());
-            m.start();
+        try
+        {
+            Thread.sleep(200);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
         }
     }
+}
+class Test implements Runnable{
+    public static Thread thread1;
+    public static Test obj;
+
+    public static void main(String[] args) {
+        obj = new Test();
+        thread1 = new Thread(obj);
+        System.out.println(thread1.getState());
+        thread1.start();
+        System.out.println(thread1.getState());
+        try{
+            thread1.sleep(200);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(thread1.getState());
+    }
+    public void run(){
+        multiThread myThread = new multiThread();
+        Thread thread2 = new Thread(myThread);
+        System.out.println(thread2.getState()+" ::State Before Start");
+        thread2.start();
+        System.out.println(thread2.getState()+" ::State after Start");
+        try{
+            thread2.sleep(200);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(thread2.getState()+" ::State after Sleep");
+    }
+
 
 }
